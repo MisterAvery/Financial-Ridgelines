0cec065947868fb5006ce7b455f058676c551ae2
MisterAvery <mister.avery99@gmail.com>
2025-07-14 14:42:03 -0400

    Added publcations page, wrote scripts for the dropdown, set up email service for integration into the stack, general UI updates

--- src/main.js
+++ src/main.js
@@ -8,6 +8,33 @@ function toggleDropdown() {
     } else {
       document.getElementById("courses-dropdown").classList.add("dropdown-active");
       document.getElementById("courses-dropdown-trigger").classList.add("selected");
+      
+      window.scrollTo(0, 0);
       dropddownDisplayed = true;
     }
 }
+
+const courseInformation = {
+  "FLOE Seminar": {
+    longName: "Financial Levers for Operational Effects Seminar",
+    description: "The FLOE seminar teaches participants how to understand and apply financial information and intelligence to support and achieve organizational effects and end states – both offensively and defensively. Students learn key concepts of preventing ‘friendly’ financial efforts from negatively impacting operations and our strategic objectives (defense), the economic impacts of the local Operating Environment, as well as exploiting and understanding adversarial efforts to raise, store, move and use financial resources to achieve desired effects (offense) working within an inter-agency and international framework.",
+    bullets: [
+      "How financial intelligence connects organizational people, operations, logistics and networks.",
+      "How ‘value’ and resourcing is generated, stored, moved and used and what opportunities or threats it can present for their mission set.",
+      "How to employ financial information to achieve operational effects.",
+      "How do protect their own unit’s financial and logistical efforts.",
+      "Inter-Agency and International perspectives, authorities and capabilities – and the necessity for a team-oriented approach spanning partner agencies.",
+      "The near-universal application of financial information to all occupational specialties and units.",
+      "The economic impacts of the operating environment upon military end states as well as military impacts to the local economy."
+    ]
+  }
+};
+
+function setDropdownInformation(shortTitle) {
+  document.getElementById("content-header").innerText = courseInformation[shortTitle].longName;
+  document.querySelector("#course-paragraph > p").innerText = courseInformation[shortTitle].description;
+  
+  document.getElementById("course-bullets").innerHTML = "<h3>End State: Students Understand</h3>" + courseInformation[shortTitle].bullets.map(bullet => { return `<li>${bullet}</li>`}).join('');
+}
+
+document.onload = setDropdownInformation("FLOE Seminar");
