879e27d94e74f0e4a9b04d2da3a6468fbe62c795
MisterAvery <mister.avery99@gmail.com>
2025-07-23 02:14:21 -0400

    Products & Training catalog update and bug patches

--- src/main.js
+++ src/main.js
@@ -17,6 +17,14 @@ function toggleDropdown() {
 let selectedTitle;
 
 const courseInformation = {
+  "Custom": {
+    longName: "Tailored Classes & Relationships",
+    description: "We strive to address unique requirements and interests of our audiences and customers. Leveraging keen relationships with trusted partners with Drug Enforcement Administration, Industry, Judicial and Academia, we can build a course or seminar to address unique needs by bringing a vast spectrum of actual experience – not just stories – to the classroom.",
+    bullets: [
+      "we can build a course or seminar to address unique needs by bringing a vast spectrum of actual experience"
+    ]
+  },
+  
   "FLOE Seminar": {
     longName: "Financial Levers for Operational Effects Seminar",
     description: "The FLOE seminar teaches participants how to understand and apply financial information and intelligence to support and achieve organizational effects and end states – both offensively and defensively. Students learn key concepts of preventing ‘friendly’ financial efforts from negatively impacting operations and our strategic objectives (defense), the economic impacts of the local Operating Environment, as well as exploiting and understanding adversarial efforts to raise, store, move and use financial resources to achieve desired effects (offense) working within an inter-agency and international framework.",
@@ -31,14 +39,24 @@ const courseInformation = {
     ]
   },
   
-  // "Custom": {
-  //   longName: "Custom",
-  //   description: "Make it up",
-  //   bullets: [
-  //     "foo",
-  //     "bar"
-  //   ]
-  // }
+  "Financial Targeting": {
+    longName: "Using Financial Understanding to Target Networks",
+    description: "This unique seminar explores the subject of leveraging financial information, people, processes, and systems for network development, understand how it is using its resources and determine indicators, warnings and potential future operations.",
+    bullets: [
+      "Great for military, intelligence, governmental and law enforcement audiences.",
+      "Examines the importance and application of determining how an organization generates, moves, uses and stores its resources",
+      "Provides an actionable framework of how to apply that understanding to support mission goals and desired endstates."
+    ]
+  },
+  
+  "Global Financial Competition": {
+    longName: "Financial Competition Around the Globe",
+    description: "This seminar explores how nations compete at that national/strategic level for influence and national strategic end states through the use of cross-border/international payment systems, currencies and the influence of Inter-governmental Financial Organizations.",
+    bullets: [
+      "examines the economic strategies that countries use to gain influence, access and power in global regions by use of economic and financial systems",
+      "Great for military, intelligence, governmental, academic and law enforcement audiences."
+    ]
+  }
 };
 
 function setDropdownInformation(event) {
@@ -52,4 +70,12 @@ function setDropdownInformation(event) {
   document.getElementById("course-bullets").innerHTML = "<h3>End State: Students Understand</h3>" + courseInformation[selectedTitle].bullets.map(bullet => { return `<li>${bullet}</li>`}).join('');
 }
 
-document.onload = setDropdownInformation({ target: document.getElementById("initial")});
+window.onload = () => {
+  for (let courseTitle in courseInformation) {
+    document.getElementById("course-selection-con").innerHTML += `<h4 class="course-short-title" id="initial" onmouseover="setDropdownInformation(event)">${courseTitle}</h4>`;
+  }
+  
+  document.getElementsByClassName("course-short-title")[0].classList.add("selected");
+  
+  setDropdownInformation({ target: document.getElementsByClassName("course-short-title")[0] });
+};
